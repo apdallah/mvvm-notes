232ac1d02d392ab3c8cae4568792d1c87da9e242
Abdallah <Ab21111392>
2019-02-03 00:51:23 +0200

    Views

--- app/src/main/java/com/example/camerates/myapplication/MainActivity.java
+++ app/src/main/java/com/example/camerates/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -19,11 +21,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        noteViewModel= ViewModelProviders.of(this).get(NoteViewModel.class);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        final NoteAdapter noteAdapter = new NoteAdapter();
+        recyclerView.setAdapter(noteAdapter);
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(@Nullable List<Note> notes) {
-              Toast.makeText(getApplicationContext(),"Changed Size: "+notes.size(),Toast.LENGTH_SHORT).show();
+                noteAdapter.setNoteList(notes);
             }
         });
     }
---
+++ app/src/main/java/com/example/camerates/myapplication/NoteAdapter.java
@@ -0,0 +1,55 @@
+package com.example.camerates.myapplication;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
+
+    private List<Note> noteList=new ArrayList<>();
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View viewitem= LayoutInflater.from(viewGroup.getContext())
+                        .inflate(R.layout.note_item,viewGroup,false);
+
+        return new NoteViewHolder(viewitem);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder noteViewHolder, int i) {
+           Note currentItem=noteList.get(i);
+           noteViewHolder.textViewTitle.setText(currentItem.getTitle());
+           noteViewHolder.textViewPeriorty.setText(String.valueOf(currentItem.getPriority()));
+           noteViewHolder.textViewDescription.setText(currentItem.getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return noteList.size();
+    }
+
+    public void setNoteList(List<Note> noteList) {
+        this.noteList = noteList;
+        notifyDataSetChanged();
+    }
+
+    class NoteViewHolder extends RecyclerView.ViewHolder{
+        TextView textViewTitle;
+        TextView textViewPeriorty;
+        TextView textViewDescription;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTitle=itemView.findViewById(R.id.textview_title);
+            textViewPeriorty=itemView.findViewById(R.id.textview_periorty);
+            textViewDescription=itemView.findViewById(R.id.textview_description);
+        }
+    }
+}
